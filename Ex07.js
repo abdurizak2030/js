a902c2d8e2ccec7019e0836f50f63d1bd8b0b194
cabdirisaaq sheekh siyaad <abdurizak2030@gmail.com>
2025-11-19 15:09:05 +0300

    exercise 07 js

---
+++ Ex07.js
@@ -0,0 +1,16 @@
+//arrys 
+//before
+let colors = ['red', 'blue', 'green', 'yellow',];
+console.log(colors [0]);
+console.log(colors [1]);
+console.log(colors [2]);
+console.log(colors [3]);
+
+
+
+//after new color added
+colors.push('black');
+
+//remove first color
+colors.shift('red');
+console.log(colors);

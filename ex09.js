f9a6f7765a0c97381875a1d44c237c9c1db0c836
cabdirisaaq sheekh siyaad <abdurizak2030@gmail.com>
2025-11-21 09:26:33 +0300

    exercise 09

---
+++ ex09.js
@@ -0,0 +1,12 @@
+let car ={
+    name: "BMW",
+    make: 2020,
+    year: 2023,
+    start: function (){
+        console.log("the car has started");
+    }
+};
+console.log(car.name);
+console.log(car.make);
+console.log(car.year);
+car.start();

14921a0b76f5aa2b78347ff284f853c8f66c2f46
cabdirisaaq sheekh siyaad <abdurizak2030@gmail.com>
2025-11-21 10:07:42 +0300

    exercise 10

---
+++ exercise10.js
@@ -0,0 +1,15 @@
+let library =[
+    {
+        title: 'The Road Ahead',
+        author: 'Bill Gates',
+        year: 1995
+    },
+    {
+        title: 'Walter Isaacson',
+        author: 'Steve Jobs',
+        year: 2011
+    },
+    
+]
+console.log(library[0].title);
+console.log(library[1].author);
